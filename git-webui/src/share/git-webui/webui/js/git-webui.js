b0afa5d0b04e6ba5ef5a4311750d92c126f28dc3
Sarmishta Velury <isc-svelury@users.noreply.github.com>
2021-10-22 09:06:12 -0400

    Fixing stale code after improper merge

--- git-webui/src/share/git-webui/webui/js/git-webui.js
+++ git-webui/src/share/git-webui/webui/js/git-webui.js
@@ -14,6 +14,10 @@
  * limitations under the License.
  */
 
+/*
+ * Modifications Copyright (C) 2021 InterSystems Corporation
+ */
+
 "use strict"
 
 var webui = webui || {};
@@ -83,7 +87,7 @@ webui.git = function(cmd, arg1, arg2) {
                 }
             }
 
-            var messageStartIndex = fIndex - parseInt(footers["Git-Stderr-Length"]);
+            var messageStartIndex = fIndex - parseInt(footers["Git-Stderr-Length"]) -1;
             var message = data.substring(messageStartIndex, fIndex);
             var output = data.substring(0, messageStartIndex);
             var rcode = parseInt(footers["Git-Return-Code"]);
@@ -92,7 +96,7 @@ webui.git = function(cmd, arg1, arg2) {
                     callback(output);
                 }
                 // Return code is 0 but there is stderr output: this is a warning message
-                if (message.length > 0) {
+                if (message.length > 1) {
                     console.log(message);
                     webui.showWarning(message);
                 }
@@ -148,17 +152,17 @@ webui.TabBox = function(buttons) {
         elt.callback();
     }
 
-    self.element = $('<ul class="nav nav-pills navbar bg-light" role="tablist">')[0];
+    self.element = $('<ul class="nav nav-pills" role="tablist">')[0];
 
     for (var i = 0; i < buttons.length; ++i) {
         var item = buttons[i];
-        var li = $('<li class="col-sm-1"><a href="#" onclick="return false;">' + item[0] + '</a></li>');
+        var li = $('<li class="nav-item"><a class="nav-link" href="#" onclick="return false;">' + item[0] + '</a></li>');
         li.appendTo(self.element);
         li.click(self.itemClicked);
         li[0].callback = item[1];
     }
-    var li = $('<li class="col-sm-'+(12-buttons.length)+'">&nbsp</li>');
-    li.appendTo(self.element);
+    // var li = $('<li class="col-sm-'+(12-buttons.length)+'">&nbsp</li>');
+    // li.appendTo(self.element);
 };
 
 /*
@@ -200,7 +204,7 @@ webui.SideBarView = function(mainView) {
                                 '<div class="modal-content">' +
                                     '<div class="modal-header">' +
                                         '<h4 class="modal-title">' + title + '</h4>' +
-                                        '<button type="button" class="btn -btn-default close" data-bs-dismiss="modal">'+
+                                        '<button type="button" class="btn btn-default close" data-bs-dismiss="modal">'+
                                         '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">'+
                                         '<path fill-rule="evenodd" clip-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z" fill="#000"/>'+
                                         '<path fill-rule="evenodd" clip-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z" fill="#000"/>'+
@@ -248,7 +252,7 @@ webui.SideBarView = function(mainView) {
                 });
             }
             if (refs.length > 0) {
-                var ul = $("<ul>").appendTo(section)[0];
+                var accordionDiv = $('<div class="accordion" id="accordion-'+id+'">').appendTo(section)[0];
                 refs = refs.sort(function(a, b) {
                     if (id != "local-branches") {
                         return -a.localeCompare(b);
@@ -273,25 +277,55 @@ webui.SideBarView = function(mainView) {
                             ref = '  ' + newref;
                         }
                     }
-                    var li = $('<li class="sidebar-ref">').appendTo(ul)[0];
+                    var cardDiv = $('<div class="accordion-item custom-accordion">').appendTo(accordionDiv)[0];
+                    // var li = $('<li class="sidebar-ref">').appendTo(ul)[0];
                     if (id == "local-branches") {
-                        li.refName = ref.substr(2);
+                        // li.refName = ref.substr(2);
+                        var refname = ref.substr(2)
+                        var cardHeader = $('<div class="accordion-header" id="heading-' + refname+'">').appendTo(cardDiv)
+                        var button = $('<button class="btn btn-sm btn-default btn-branch text-left" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-'+ refname+'" aria-expanded="true" aria-controls="collapse-'+ refname+'">'
+                                        + refname
+                                    + '</button>').appendTo(cardHeader);
+                        
+                        if(ref[0] != "*") {
+                            var collapseDiv = $('<div id="collapse-'+ refname+'" class="accordion-collapse collapse" aria-labelledby="heading-' + refname+'" data-bs-parent="#accordion-'+id+'">').appendTo(cardDiv);
+                            var cardBody = $('<div class="accordion-body">' +
+                                            '<div class="d-grid gap-2 col-12 mx-auto">'+
+                                                '<button class="btn btn-xs btn-primary btn-block btn-checkout-local-branch">Checkout Branch</button>'+
+                                                '<button class="btn btn-xs btn-danger btn-block btn-delete-branch">Delete Branch</button>'+
+                                            '</div>'+
+                                        '</div>').appendTo(collapseDiv);
+                        }
+                        
                         if (ref[0] == "*") {
-                            $(li).addClass("branch-current");
+                            $(button).addClass("branch-current");
                             window.setTimeout(function() {
                                 var current = $(".branch-current", self.element)[0];
                                 if (current) {
-                                    self.selectRef(current.refName);
+                                    self.selectRef(current.innerHTML);
                                 }
                             }, 0);
                         }
+
                     } else {
-                        li.refName = ref;
+                        var refname = ref.replaceAll('/', '-');
+                        var cardHeader = $('<div class="accordion-header" id="heading-' + refname+'">').appendTo(cardDiv)
+                        var button = $('<button class="btn btn-sm btn-default btn-branch text-left" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-'+ refname+'" aria-expanded="true" aria-controls="collapse-'+ refname+'">'
+                                        + ref //IMPORTANT: This has to be the original ref for selectRef to work 
+                                    + '</button>').appendTo(cardHeader)
+
+                        var collapseDiv = $('<div id="collapse-'+ refname+'" class="collapse" aria-labelledby="heading-' + refname+'" data-bs-parent="#accordion-'+id+'">').appendTo(cardDiv);
+                        var cardBody = $('<div class="card-body">' +
+                                        '<div class="d-grid gap-2 col-12 mx-auto">'+
+                                            '<button class="btn btn-xs btn-primary btn-block btn-checkout-remote-branch">Checkout Branch</button>'+
+                                        '</div>'+
+                                        '</div>').appendTo(collapseDiv);
                     }
-                    $(li).attr("title", li.refName);
-                    $(li).text(li.refName);
-                    $(li).click(function (event) {
-                        self.selectRef(event.target.refName);
+                    // $(button).attr("title", li.refName);
+                    $(button).click(function (event) {
+                        // $(button).removeClass("collapsed");
+                        // $(button).siblings().closest().addClass("show")
+                        self.selectRef(event.target.innerHTML);
                     });
                 }
 
@@ -316,10 +350,15 @@ webui.SideBarView = function(mainView) {
                                     '<h4>Workspace</h4>' +
                                 '</section>' +
                                 '<section id="sidebar-local-branches">' +
-                                    '<h4>Local Branches</h4>' +
+                                    '<h4 class="mt-3">Local Branches' +
+                                    '<button type="button" class="btn btn-default btn-add" >' +
+                                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#eee" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">'+
+                                            '<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>'+
+                                        '</svg>'+
+                                    '</button>' + '</h4>' +
                                 '</section>' +
                                 '<section id="sidebar-remote-branches">' +
-                                    '<h4>Remote Branches</h4>' +
+                                    '<h4 class="mt-3">Remote Branches</h4>' +
                                 '</section>' +
                                 '<section id="sidebar-tags">' +
                                     '<h4>Tags</h4>' +
@@ -1735,4 +1774,84 @@ function MainUi() {
 
 $(document).ready(function () {
     new MainUi()
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+$(function()
+{
+    $(document).on('click', '.btn-add', function(e)
+    {
+        e.preventDefault();
+
+        var controlForm = $('#sidebar-local-branches');
+        // $('<div class="col-sm-3" id="indent-div"> </div>').appendTo(controlForm);
+
+        // var currentEntry = $(this).children('ul').children()('.sidebar:last');
+        var inputForm = '<button type="submit" class="btn btn-md btn-default btn-ok" id="btn_createList">' +
+                            '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#eeeeee" class="bi bi-check2" viewBox="0 0 16 16">'+
+                                '<path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>'+
+                            '</svg>'+
+                        '</button>' +
+                        '<input type="text" class="form-control form-control-xs" id="newBranchName"/>'
+        controlForm.append(inputForm);
+
+        // newEntry.find('input').val('');
+        //controlForm.find('.btn-add:not(:last)')
+            //.removeClass('btn-default').addClass('btn-danger')
+            //.removeClass('btn-add').addClass('btn-remove')
+            
+            //.html('<span class="glyphicon glyphicon-minus" aria-hidden="true"></span> Remove   ');
+    }).on('click', '.btn-remove', function(e)
+    {
+        $(this).parent().parent().parent().prev('#indent-div').remove();
+        $(this).parents('.voca:first').remove();
+
+        e.preventDefault();
+        return false;
+    }).on('click', '#btn_createList', function(e)
+    {   
+        webui.git("checkout -b " + $('#newBranchName').val());
+        $('#sidebar-local-branches ul').append($('<li>', {
+            text: $('#newBranchName').val()
+        }));
+        $('#sidebar-local-branches input').remove();
+        $('#sidebar-local-branches .btn-ok').remove()
+    });
+});
+
+$(function () {
+
+    $(document).on('click', '.btn-checkout-local-branch', function(e) {
+        e.preventDefault();
+        var refName = $(this).parent().parent().parent().siblings(
+            ".accordion-header").children("button").html();
+
+        webui.git("checkout " + refName);
+        
+    });
+
+    $(document).on('click', '.btn-delete-branch', function(e) {
+        e.preventDefault();
+        var refName = $(this).parent().parent().parent().siblings(
+            ".accordion-header").children("button").html();
+
+        webui.git("branch -d " + refName);
+        webui.showWarning("Local branch "+refName+" deleted.")        
+    });
+
+    $(document).on('click', '.btn-checkout-remote-branch', function(e) {
+        e.preventDefault();
+        var refName = $(this).parent().parent().parent().siblings(
+            ".accordion-header").children("button").html();
+
+        var remoteName = refName.split('/')[0];
+        var branchName = refName.split('/')[1];
+        
+        webui.git("fetch "+remoteName);
+        webui.git("checkout -b " + branchName + " " + refName);
+        
+    });
+
 });

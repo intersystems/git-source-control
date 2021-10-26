8bdb3256cbaf365d3304fd5f0543e8daead9fe0f
Sarmishta Velury <isc-svelury@users.noreply.github.com>
2021-10-26 16:04:32 -0400

    Added icon to alert

--- git-webui/src/share/git-webui/webui/js/git-webui.js
+++ git-webui/src/share/git-webui/webui/js/git-webui.js
@@ -1829,7 +1829,7 @@ $(function () {
                             '<div class="modal-dialog modal-md">' +
                                 '<div class="modal-content">' +
                                     '<div class="modal-header">' +
-                                        '<h5 class="modal-title">Confirm <pre> '+refName+' </pre>Deletion</h5>' +
+                                        '<h5 class="modal-title">Confirm <pre>'+refName+' </pre>Deletion</h5>' +
                                         '<button type="button" class="btn btn-default close" data-dismiss="modal">'+
                                         '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">'+
                                         '<path fill-rule="evenodd" clip-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z" fill="#000"/>'+
@@ -1845,7 +1845,11 @@ $(function () {
         $("body").append(popup); 
         var popupContent = $(".modal-body", popup)[0];
         
-        $("<p>By deleting this branch, you might lose uncommitted work. <br/> Do you want to proceed?</p>"+
+        $('<div class="row"><div class="col-sm-1">'+
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#dc3545" class="bi bi-exclamation-circle-fill" viewBox="0 0 16 16">'+
+            '<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path>'+
+        '</svg></div>'+
+        '<div class="col-sm-11">By deleting this branch, you might lose uncommitted work. <br/> Do you want to proceed?</div></div>'+
         '<button class="btn btn-sm btn-danger float-right" id="confirm-delete">Delete Branch</button>'+
         '<button class="btn btn-sm btn-secondary float-right" id="cancel-delete">Cancel</button>').appendTo(popupContent);
         $(popup).modal('show');

bf1281c45bd0b0bfc746ae6910bbcc912b324d7c
isc-svelury <91484958+isc-svelury@users.noreply.github.com>
2021-11-05 11:53:14 -0400

    Prune remote branches (#59)
    * Refresh button added to Remote Branches section that prunes refs to deleted remote branches.
    
    * Propagated changes to src/
    
    Co-authored-by: Sarmishta Velury <isc-svelury@users.noreply.github.com>

--- git-webui/src/share/git-webui/webui/js/git-webui.js
+++ git-webui/src/share/git-webui/webui/js/git-webui.js
@@ -370,14 +370,20 @@ webui.SideBarView = function(mainView) {
                                 '</section>' +
                                 '<section id="sidebar-local-branches">' +
                                     '<h4 class="mt-3">Local Branches' +
-                                    '<button type="button" class="btn btn-default btn-add" >' +
+                                    '<button type="button" class="btn btn-default btn-sidebar-icon btn-add" >' +
                                         '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#eee" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">'+
                                             '<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>'+
                                         '</svg>'+
                                     '</button>' + '</h4>' +
                                 '</section>' +
                                 '<section id="sidebar-remote-branches">' +
-                                    '<h4 class="mt-3">Remote Branches</h4>' +
+                                    '<h4 class="mt-3">Remote Branches' +
+                                    '<button type="button" class="btn btn-default btn-sidebar-icon btn-prune-remote-branches" >'+
+                                        '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="#eee" class="bi bi-arrow-repeat" viewBox="0 0 16 16">'+
+                                        '<path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>'+
+                                        '<path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>'+
+                                        '</svg>'+
+                                      '</button>' +'</h4>' +
                                 '</section>' +
                                 '<section id="sidebar-tags">' +
                                     '<h4>Tags</h4>' +
@@ -1839,6 +1845,13 @@ $(function()
         webui.git("checkout -b " + $('#newBranchName').val());
         updateSideBar();
     });
+
+    $(document).on('click', '.btn-sidebar-icon', function(e){
+        e.preventDefault();
+        webui.git("fetch --prune", function() {
+            updateSideBar();
+        });
+    })
 });
 
 $(function () {
